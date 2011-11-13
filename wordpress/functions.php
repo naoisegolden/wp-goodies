b093c95d9548e4c33338598697497139205a60d3
Naoise Golden Santos <naoise.golden@gmail.com>
2011-11-13 23:35:33 +0100

    diable wpml language selector css

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -1,4 +1,7 @@
 // default image link to none
 update_option('image_default_link_type' , '');
 
+// disable WPML's language selector css
+define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
+
 // more to come...
