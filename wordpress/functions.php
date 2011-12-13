50145c20c4d4258ed9b53d01c04d1c0e6afd1687
Naoise Golden Santos <naoise.golden@gmail.com>
2011-12-13 03:49:40 +0100

    add language to body class snippet

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -4,4 +4,11 @@ update_option('image_default_link_type' , '');
 // disable WPML's language selector css
 define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
 
+// add language to body class
+function extend_body_class($c) {
+  $c[] = 'lang-' . ICL_LANGUAGE_CODE;
+	return $c;
+}
+add_filter('body_class', 'extend_body_class');
+
 // more to come...
